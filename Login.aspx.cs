531cf9711f2e9ad731e630746c958a61d108f78c
Abhishek Arun <abhishekarun021@gmail.com>
2023-03-05 15:34:31 +0530

    Login

---
+++ Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+
+namespace _3_TierP
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        LogCls objbll = new LogCls();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cid = objbll.LogDB(TextBox1.Text, TextBox2.Text);
+            if (cid == "1")
+            {
+                Label1.Text = "Logged In";
+            }
+            else
+            {
+                Label1.Text = "Register";
+            }
+        }
+    }
+}
